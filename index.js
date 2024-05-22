b52d950cbc2bc442575c5c780d47af3393e2c7ad
Jeffrey <j.lo128456@gmail.com>
2024-05-23 09:55:29 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// function to return an array of thank you messages for each name
+function writeCards(namesArray, eventName) 
+{
+    let messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) 
+    {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messagesArray.push(message);
+    }
+    return messagesArray;
+}
+//function to perfoprm a count down
+function countDown(number)
+{
+  let i = number
+  while (i >= 0)
+    {
+      console.log(i);
+      i--;
+    }
+
+
+}
